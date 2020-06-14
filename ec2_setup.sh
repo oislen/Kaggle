8116982de4a0878ffd97475b035e2f7bf47d5069
oislen <oisin.leonard@gmail.com>
2020-06-14 20:33:49 +0100

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -9,9 +9,14 @@
 # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/putty.html
 # https://docs.aws.amazon.com/transfer/latest/userguide/getting-started-use-the-service.html
 # https://aws.amazon.com/premiumsupport/knowledge-center/ec2-linux-2-install-gui/
+# note: winscp sftp settings: sudo /usr/lib/openssh/sftp-server
 
 # check available memory
 free
+df
+
+# reset premission for the instance
+sudo chmod -R 777 /.
 
 # install git
 sudo yum update
@@ -23,7 +28,7 @@ git clone https://github.com/oislen/AWS.git
 # configure vim
 sudo yum install vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp AWS/.vimrc ~/.
+cp ~/AWS/.vimrc ~/.
 vim AWS/ec2_setup.sh
 :PluginInstall
 :q
@@ -42,20 +47,28 @@ yes
 # configure conda
 export PATH=~/anaconda3/bin:$PATH
 conda init bash
+conda config --set auto_activate_base false
 # reset terminal
 
 # create aws environment
 conda create --name aws
 conda activate aws
-conda install pandas
-conda install scipy
-conda install scikit-learn
-conda install statsmodels
-conda install seaborn
+conda install --yes pandas
+conda install --yes scipy
+conda install --yes scikit-learn
+conda install --yes statsmodels
+conda install --yes seaborn
 pip install pygam
 #conda env export > aws.yml
 #conda env create -f aws.yml
 # reset terminal
+
+# create kaggle scripts & data
+# upload raw data to ec2
+sudo git clone https://github.com/oislen/Kaggle.git
+sudo mkdir /opt/Kaggle/Predict_Future_Sales/data
+sudo mkdir /opt/Kaggle/Predict_Future_Sales/report
+sudo cp -rf /home/ec2-user/raw/ /opt/Kaggle/Predict_Future_Sales/data/
 
 # install tigervnc
 sudo yum update -y
