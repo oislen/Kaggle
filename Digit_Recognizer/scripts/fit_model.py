85e1855ba9b03aa1d5822d43ed84864fe3e8e9fc
oislen <oisin.leonard@gmail.com>
2021-01-31 20:08:44 +0000

    modifed code to work without data augmentation

--- Digit_Recognizer/scripts/fit_model.py
+++ Digit_Recognizer/scripts/fit_model.py
@@ -6,23 +6,22 @@ Created on Sun Jan 31 15:31:42 2021
 """
 
 import os
-from keras.optimizers import RMSprop #, Adam
 from keras.callbacks import ModelCheckpoint
 from graph import plot_history
 
 def fit_model(model,
-              datagen, 
               X_train, 
               Y_train,
               X_val, 
               Y_val, 
               batch_size,
-              model_name=None,
-              output_dir="data/checkpoints",
-              class_weight=None, 
-              epochs=60, 
-              lr=0.001, 
-              verbose=False
+              optimizer,
+              datagen = None, 
+              model_name = None,
+              output_dir = "data/checkpoints",
+              class_weight = None, 
+              epochs = 60, 
+              verbose = False
               ):
     """
     
@@ -35,24 +34,37 @@ def fit_model(model,
     if verbose:
         model.summary()
     
-    # define the optimiser to use
-    #optimizer = Adam(lr = lr)
-    optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-    
     # compile model (can use another optimizer)
-    model.compile(optimizer = optimizer,#Adam(lr=lr),
+    model.compile(optimizer = optimizer,
                   loss = 'categorical_crossentropy',
                   metrics = ['accuracy']
                  )
     
+    if datagen == None:
+        validation_data = (X_val, Y_val)
+        x = X_train 
+        y = Y_train
+    else:
+        validation_data = (datagen.standardize(X_val), Y_val)
+        x = datagen.flow(X_train, Y_train, batch_size=batch_size)
+        y = None
+    
+    # calculate steps per epoch
+    steps_per_epoch = len(X_train) // batch_size
+    
+    # generate checkpoints
+    check_points = [ModelCheckpoint(os.path.join(output_dir, "{model_name}").format(model_name=model_name) + "-{epoch:02d}-{val_loss:.2f}.hdf5", save_best_only=True),] if model_name is not None else []
+    
     # starts training
-    history = model.fit(datagen.flow(X_train, Y_train, batch_size=batch_size),
-                        validation_data = (datagen.standardize(X_val), Y_val),
+    history = model.fit(x = x,
+                        validation_data = validation_data,
+                        y = y,
                         epochs = epochs, 
-                        steps_per_epoch=len(X_train) // batch_size,
-                        callbacks = [ModelCheckpoint(os.path.join(output_dir, "{model_name}").format(model_name=model_name) + "-{epoch:02d}-{val_loss:.2f}.hdf5", save_best_only=True),
-                                     ] if model_name is not None else [],
+                        steps_per_epoch = steps_per_epoch,
+                        callbacks = check_points,
                         class_weight = class_weight
                         )  
     
     plot_history(history)
+    
+    return 0
