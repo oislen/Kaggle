5ab148d2723169a209d2f67e9c17325d0978033c
oislen <oisin.leonard@gmail.com>
2021-01-31 13:10:34 +0000

    added additional programme constants and file paths

--- Digit_Recognizer/scripts/cons.py
+++ Digit_Recognizer/scripts/cons.py
@@ -13,6 +13,8 @@ comp_name = 'digit-recognizer'
 download_data = True
 unzip_data = True
 del_zip = True
+random_state = 1234
+valid_size = 0.1
 
 # set directories
 root_dir = 'C:\\Users\\User\\Documents\\GitHub\\Kaggle\\Digit_Recognizer'
@@ -21,6 +23,13 @@ data_dir = os.path.join(root_dir, 'data')
 
 # define filenames
 zip_data_fname = '{}.zip'.format(comp_name)
+sample_sub_data_fname = 'sample_submission.csv'
+test_data_fname = 'test.csv'
+train_data_fname = 'train.csv'
 
 # create file paths
 zip_data_fpath = os.path.join(data_dir, zip_data_fname)
+sample_sub_data_fpath = os.path.join(data_dir, sample_sub_data_fname)
+test_data_fpath = os.path.join(data_dir, test_data_fname)
+train_data_fpath = os.path.join(data_dir, train_data_fname)
+
