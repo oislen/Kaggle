cd1ebc185540c6a6d4cf2d64c4a975ab5facb785
oislen <oisin.leonard@gmail.com>
2021-02-04 20:48:53 +0000

    added call to retrieve submission results following kaggle submission

--- competitions/Digit_Recognizer/scripts/03_submt_digit_preds.py
+++ competitions/Digit_Recognizer/scripts/03_submt_digit_preds.py
@@ -8,9 +8,13 @@ Created on Tue Feb  2 14:42:09 2021
 # load relevant libraries
 import cons
 from sub_comp_preds import sub_comp_preds
+from get_comp_subs import get_comp_subs
 
 # submit model predictions
 sub_comp_preds(comp_name = cons.comp_name,
                pred_data_fpath = cons.pred_data_fpath,
                sub_mess = "testing kaggle api"
                )
+
+# get submission results
+get_comp_subs(comp_name = 'digit-recognizer')
