dffaa750fff08e50cbcc9861f3466261a7c6e927
oislen <oisin.leonard@gmail.com>
2021-02-08 19:30:19 +0000

    created titanic classification submission script

---
+++ competitions/Titanic/scripts/07_submit_titanic_preds.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 19:26:42 2021
+
+@author: oislen
+"""
+
+# load relevant libraries
+import cons
+from sub_comp_preds import sub_comp_preds
+from get_comp_subs import get_comp_subs
+
+# submit model predictions
+sub_comp_preds(comp_name = cons.comp_name,
+               pred_data_fpath = cons.pred_data_fpath,
+               sub_mess = "testing kaggle api"
+               )
+
+# get submission results
+get_comp_subs(comp_name = cons.comp_name)
