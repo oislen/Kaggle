b96f5dc876e0513cf4d5ac8ea69df8881a8307fa
oislen <oisin.leonard@gmail.com>
2021-02-10 18:01:16 +0000

    generalised script to work with final classification model and age imputation model

--- competitions/Titanic/scripts/utilities/fit_mod.py
+++ competitions/Titanic/scripts/utilities/fit_mod.py
@@ -7,49 +7,58 @@ Created on Tue Feb  9 15:10:47 2021
 
 # load in relevant libraries
 import pandas as pd
-from sklearn import ensemble
+import cons
 import value_analysis as va
 
-def age_nafill_mod(base_train,
-                   base_test,
-                   y_col,
-                   X_col,
-                   params,
-                   random_state = 123,
-                   train_size = 0.8,
-                   test_size = 0.2,
-                   random_split = True,
-                   sample_target = None,
-                   scoring = 'neg_mean_squared_error',
-                   cv = 10,
-                   n_jobs = -1,
-                   refit = True,
-                   return_mod = True,
-                   verbose = 0
-                   ):
+def fit_mod(base_train,
+            base_test,
+            y_col,
+            X_col,
+            model,
+            params,
+            target_type = 'reg',
+            random_state = 123,
+            train_size = 0.8,
+            test_size = 0.2,
+            random_split = True,
+            sample_target = None,
+            sample_type = 'over',
+            scoring = 'neg_mean_squared_error',
+            cv = 10,
+            n_jobs = -1,
+            refit = True,
+            return_mod = True,
+            verbose = 0
+            ):
     
     """
     
-    Age NA Fill Model Documentation
+    Fit Model Documentation
     
     Function Overview
     
-    This function fills in the missing age values for the Titanic base data
+    This function fits sklearn model for the Titanic compeition
     
     Defaults
     
-    age_nafill_mod(base_train,
-                   base_test,
-                   y_col,
-                   X_col,
-                   params,
-                   random_state = 123,
-                   train_size = 0.8,
-                   test_size = 0.2,
-                   random_split = True,
-                   sample_target = None,
-                   scoring = 'neg_mean_squared_error'
-                   )
+    fit_mod(base_train,
+            base_test,
+            y_col,
+            X_col,
+            model,
+            params,
+            random_state = 123,
+            train_size = 0.8,
+            test_size = 0.2,
+            random_split = True,
+            sample_target = None,
+            scoring = 'neg_mean_squared_error',
+            cv = 10,
+            n_jobs = -1,
+            refit = True,
+            return_mod = True,
+            verbose = 0
+            )
     
     Parameters
     
@@ -70,21 +79,32 @@ def age_nafill_mod(base_train,
     base - DataFrame, the base data with filled age column
     
     Example
+ 
+    fit_mod(base_train = train,
+            base_test = test,
+            y_col = ['Age'],
+            X_col = ['Pclass', 'SibSp', 'Parch', 'FamSize', 'Fare', 'Alone', 'Mr', 'Mrs', 'Ms', 'Priv', 'male', 'Embarked'],
+            params = cons.test_age_gbm_params,
+            random_state = 123,
+            train_size = 0.8,
+            test_size = 0.2,
+            random_split = True,
+            sample_target = None,
+            scoring = 'neg_mean_squared_error',
+            cv = 10,
+            n_jobs = -1,
+            refit = True,
+            return_mod = True,
+            verbose = 0
+            )
     
-    age_nafill_mod(base_train = train,
-                   base_test = test,
-                   y_col = ['Age'],
-                   X_col = ['Pclass', 'SibSp', 'Parch', 'FamSize', 'Fare', 'Alone', 'Mr', 'Mrs', 'Ms', 'Priv', 'male', 'Embarked'],
-                   params = cons.test_age_gbm_params,
-                   random_state = 123,
-                   train_size = 0.8,
-                   test_size = 0.2,
-                   random_split = True,
-                   sample_target = None,
-                   scoring = 'neg_mean_squared_error'
-                   )
-        
     """
+    
+    # extract out target
+    tar_col = y_col[0]
+    
+    # create predicted column name
+    pred_col = '{}_pred'.format(tar_col)
     
     # randomly split the dataset
     X_valid, X_train, y_valid, y_train = va.train_test_split_sample(dataset = base_train,
@@ -93,14 +113,12 @@ def age_nafill_mod(base_train,
                                                                     train_size = train_size,
                                                                     test_size = test_size,
                                                                     random_split = random_split,
-                                                                    sample_target = sample_target
+                                                                    sample_target = sample_target,
+                                                                    sample_type = sample_type
                                                                     )
     
-    # initiate gbm
-    gbm = ensemble.GradientBoostingRegressor(random_state = random_state)
-    
     # tune gbm model
-    mod_tuning = va.tune_hyperparameters(model = gbm, 
+    mod_tuning = va.tune_hyperparameters(model = model, 
                                          params = params, 
                                          X_train = X_train, 
                                          y_train = y_train,
@@ -112,8 +130,10 @@ def age_nafill_mod(base_train,
                                          verbose = verbose
                                          )
     
+    # if not refitting or retunring the best model
     if refit == False or return_mod == False:
         
+        """
         # extract out the model tuning results
         mod_tuning_df = mod_tuning['tune_df']
         
@@ -133,47 +153,82 @@ def age_nafill_mod(base_train,
         gbm.fit(X_train, 
                 y_train.values.ravel()
                 )
+        """
+       
+        raise ValueError('wrong inputs')
         
+    # otherwise
     else:
         
         # extract out the model of best fit
         gbm = mod_tuning['best_estimator']
         
     # classify the validation set
-    y_valid['Age_pred'] = gbm.predict(X_valid)
+    y_valid[pred_col] = gbm.predict(X_valid)
     
     # genrate the regression metrics
-    va.perf_metrics(y_obs = y_valid['Age'], 
-                    y_pred = y_valid['Age_pred'], 
-                    target_type = 'reg'
+    va.perf_metrics(y_obs = y_valid[tar_col], 
+                    y_pred = y_valid[pred_col], 
+                    target_type = target_type
                     )
     
-    # create prediction, observation and residual plots
-    va.Vis.preds_obs_resids(obs = 'Age',
-                            preds = 'Age_pred',
-                            dataset = y_valid
-                            )
-    
-    # plot predicted age
-    va.Vis.hist(dataset = y_valid,
-                num_var = ['Age_pred'],
-                title = 'Histogram of Predicted Age - Validation Set'
+    if target_type == 'class':
+        
+        # randomly split the dataset
+        X_valid_emp, X_train, y_valid_emp, y_train = va.train_test_split_sample(dataset = base_train,
+                                                                                y = y_col,
+                                                                                X = X_col,
+                                                                                train_size = 1,
+                                                                                test_size = 0,
+                                                                                random_split = False,
+                                                                                sample_target = sample_target,
+                                                                                sample_type = sample_type
+                                                                                )
+        
+        # refit model to all training data
+        gbm.fit(X_train[X_col], 
+                y_train[y_col].values.ravel()
                 )
-    
-    # refit model to all training data
-            # fit the best model
-    gbm.fit(base_train[X_col], 
-            base_train[y_col].values.ravel()
-            )
+        
+    else :
+        
+        # refit model to all training data
+        gbm.fit(base_train[X_col], 
+                base_train[y_col].values.ravel()
+                )
 
     # predict for the base_test set
-    base_test['Age'] = gbm.predict(base_test[X_col])
+    base_test[tar_col] = gbm.predict(base_test[X_col])
     
-    # plot predicted age
-    va.Vis.hist(dataset = base_test,
-                num_var = ['Age'],
-                title = 'Histogram of Predicted Age - Test Set'
-                )
+    # if running regression
+    if target_type == 'reg':
+            
+        # create prediction, observation and residual plots
+        va.Vis.preds_obs_resids(obs = tar_col,
+                                preds = pred_col,
+                                dataset = y_valid
+                                )
+        
+        # plot predicted age
+        va.Vis.hist(dataset = y_valid,
+                    num_var = [pred_col],
+                    title = 'Histogram of Predicted {} - Validation Set'.format(tar_col)
+                    )
+        
+        # plot predicted age
+        va.Vis.hist(dataset = base_test,
+                    num_var = [tar_col],
+                    title = 'Histogram of Predicted {} - Test Set'.format(tar_col)
+                    )
+    
+    # otherwise if classification
+    elif target_type == 'class':
+        
+        # create a ROC curve
+        va.Vis.roc_curve(obs = tar_col, 
+                         preds = pred_col, 
+                         dataset = y_valid
+                         )
     
     # re-concatenate the base training and base test to update base data
     base = pd.concat(objs = [base_train, base_test],
