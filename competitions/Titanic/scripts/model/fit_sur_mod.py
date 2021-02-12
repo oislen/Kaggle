0e50d799cd0c1d76fbfc698b90aefb48c80def1a
oislen <oisin.leonard@gmail.com>
2021-02-12 12:06:32 +0000

    moved model specific scripts to seperate directory

--- competitions/Titanic/scripts/model/fit_sur_mod.py
+++ competitions/Titanic/scripts/model/fit_sur_mod.py
@@ -9,7 +9,6 @@ Created on Tue Feb  9 15:10:47 2021
 import pandas as pd
 import cons
 import value_analysis as va
-import os
 import joblib
 from vis_feat_imp import vis_feat_imp
 
@@ -32,7 +31,6 @@ def fit_sur_mod(base_train,
                 n_jobs = -1,
                 refit = True,
                 return_mod = True,
-                report_dir = None,
                 verbose = 0
                 ):
     
@@ -113,13 +111,13 @@ def fit_sur_mod(base_train,
     pred_col = '{}_pred'.format(tar_col)
     
     # define the histogram of valid predictions filename
-    hist_train_tar_fname = '{}_hist_train_tar.png'.format(model_name)
+    
     
     # create count plot of classifications
     va.Vis.hist(dataset = base_train,
                  num_var = [tar_col],
-                 output_dir = report_dir,
-                 output_fname = hist_train_tar_fname
+                 output_dir = cons.model_results_dir.format(model_name),
+                 output_fname = cons.hist_train_tar_fname.format(model_name)
                  )
     
     print('splitting data into training and validation sets ...')
@@ -152,31 +150,16 @@ def fit_sur_mod(base_train,
     # extract out the model tuning results
     mod_tuning_df = mod_tuning['tune_df']
     
-    # define the filename
-    hyperparam_fname = '{}_hyperparam_tuning.csv'.format(model_name)
-    
-    # definte the output file path
-    hyper_param_fpath = os.path.join(report_dir, hyperparam_fname)
-    
     # save the tuning results
-    mod_tuning_df.to_csv(hyper_param_fpath,
+    mod_tuning_df.to_csv(cons.hyper_param_fpath.format(model_name, model_name),
                          index = False
                          )
 
     # extract out the model of best fit
     best_model = mod_tuning['best_estimator']
     
-    # define the filename
-    best_model_fname = '{}_best_model.pkl'.format(model_name)
-    
-    # model file path
-    best_model_fpath = os.path.join(report_dir, best_model_fname)
-    
     # pickle the best model
-    joblib.dump(best_model, best_model_fpath)
-    
-    # definte the filename
-    learning_curve_fnamt = '{}_learning_curve.png'.format(model_name)
+    joblib.dump(best_model, cons.best_model_fpath.format(model_name, model_name))
     
     # create learning curve
     va.Vis.learning_curve(model = best_model,
@@ -184,57 +167,51 @@ def fit_sur_mod(base_train,
                           y_train = y_train,
                           scoring = 'accuracy',
                           title = 'Learning Curve: {}'.format(model_name.upper()),
-                          output_dir = report_dir,
-                          output_fname = learning_curve_fnamt
+                          output_dir = cons.model_results_dir.format(model_name),
+                          output_fname = cons.learning_curve_fnamt.format(model_name)
                           )
     
     if model_name in ['rfc', 'abc', 'etc', 'gbc']:
         
         # plot feature importance
         vis_feat_imp(name = model_name, model = best_model, X_train = X_train)
+       
+    # if there is a spare validation set
+    if y_valid.shape[0] > 0:
+            
+        print('predicting for validation set ...')
         
-    print('predicting for validation set ...')
+        # classify the validation set
+        y_valid[pred_col] = best_model.predict(X_valid)
+        
+        print('evaluating validation predictions ...')
+        
+        # create count plot of classifications
+        va.Vis.hist(dataset = y_valid,
+                    num_var = [pred_col],
+                    output_dir = cons.model_results_dir.format(model_name),
+                    output_fname = cons.hist_valid_preds_fname.format(model_name)
+                    )
     
-    # classify the validation set
-    y_valid[pred_col] = best_model.predict(X_valid)
-    
-    print('evaluating validation predictions ...')
-    
-    # define the histogram of valid predictions filename
-    hist_valid_preds_fname = '{}_hist_valid_preds.png'.format(model_name)
-    
-    # create count plot of classifications
-    va.Vis.hist(dataset = y_valid,
-                num_var = [pred_col],
-                output_dir = report_dir,
-                output_fname = hist_valid_preds_fname
-                )
-    
-    # definte the metrics filename
-    metrics_fname = '{}_perf_metrics.csv'.format(model_name)
-    
-    # genrate the regression metrics
-    val_metrics = va.perf_metrics(y_obs = y_valid[tar_col], 
-                                  y_pred = y_valid[pred_col], 
-                                  target_type = target_type,
-                                  output_dir = report_dir,
-                                  output_fname = metrics_fname
-                                  )
-    
-    # print the validation metrics
-    print(val_metrics)
-    
-    # define the roc filename
-    roc_fname = '{}_roc_curve.png'.format(model_name)
-    
-    # create a ROC curve
-    va.Vis.roc_curve(obs = tar_col, 
-                     preds = pred_col, 
-                     dataset = y_valid,
-                     output_dir = report_dir,
-                     output_fname = roc_fname
-                     )
-    
+        # genrate the regression metrics
+        val_metrics = va.perf_metrics(y_obs = y_valid[tar_col], 
+                                      y_pred = y_valid[pred_col], 
+                                      target_type = target_type,
+                                      output_dir = cons.model_results_dir.format(model_name),
+                                      output_fname = cons.metrics_fname.format(model_name)
+                                      )
+        
+        # print the validation metrics
+        print(val_metrics)
+        
+        # create a ROC curve
+        va.Vis.roc_curve(obs = tar_col, 
+                         preds = pred_col, 
+                         dataset = y_valid,
+                         output_dir = cons.model_results_dir.format(model_name),
+                         output_fname = cons.roc_fname.format(model_name)
+                         )
+        
     print('refitting to all training data ...')
     
     # refit model to all training data
@@ -247,14 +224,11 @@ def fit_sur_mod(base_train,
     # predict for the base_test set
     base_test[tar_col] = best_model.predict(base_test[X_col])
     
-    # define the histogram of valid predictions filename
-    hist_test_preds_fname = '{}_hist_test_preds.png'.format(model_name)
-    
     # create count plot of classifications
     va.Vis.hist(dataset = base_test,
                 num_var = [tar_col],
-                output_dir = report_dir,
-                output_fname = hist_test_preds_fname
+                output_dir = cons.model_results_dir.format(model_name),
+                output_fname = cons.hist_test_preds_fname.format(model_name)
                 )
     
     # re-concatenate the base training and base test to update base data
