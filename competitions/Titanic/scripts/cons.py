9c78ced2b67a64c8610aef60616afea50b0a5af2
oislen <oisin.leonard@gmail.com>
2021-02-12 12:03:47 +0000

    merged model constants into cons.py

--- competitions/Titanic/scripts/cons.py
+++ competitions/Titanic/scripts/cons.py
@@ -9,10 +9,14 @@ Created on Mon Feb  8 17:20:45 2021
 import os
 import sys
 import pandas as pd
+from sklearn import ensemble, tree, svm
 
+# set pandas behaviour
 pd.set_option('display.max_columns', 20)
 
+##########################
 #-- Filepath Constants --#
+##########################
 
 # set programme constants
 comp_name = 'titanic'
@@ -34,7 +38,7 @@ univar_stats_dir = os.path.join(report_dir, 'univariate_analysis\\descriptive_st
 bivar_assoc_dir = os.path.join(report_dir, 'bivariate_analysis\\association_tests')
 bivar_corr_dir = os.path.join(report_dir, 'bivariate_analysis\\correlation_tests')
 bivar_gains_dir = os.path.join(report_dir, 'bivariate_analysis\\measure_gains')
-model_results_dir = os.path.join(report_dir, 'model_results')
+model_results_dir = os.path.join(report_dir, 'model_results\\{}')
 
 # define filenames
 zip_data_fname = '{}.zip'.format(comp_name)
@@ -46,6 +50,14 @@ base_clean_data_fname = 'base_clean.csv'
 base_clean_2_data_fname = 'base_clean_2.csv'
 base_engin_fname = 'base_engin.csv'
 pred_data_fname = '{}_preds.csv'
+hyperparam_fname = '{}_hyperparam_tuning.csv'
+best_model_fname = '{}_best_model.pkl'
+hist_train_tar_fname = '{}_hist_train_tar.png'
+learning_curve_fnamt = '{}_learning_curve.png'
+hist_valid_preds_fname = '{}_hist_valid_preds.png'
+metrics_fname = '{}_perf_metrics.csv'
+roc_fname = '{}_roc_curve.png'
+hist_test_preds_fname = '{}_hist_test_preds.png'
 
 # create file paths
 zip_data_fpath = os.path.join(data_dir, zip_data_fname)
@@ -58,82 +70,94 @@ base_clean_2_data_fpath = os.path.join(data_dir, base_clean_2_data_fname)
 base_engin_data_fpath = os.path.join(data_dir, base_engin_fname)
 pred_data_fpath = os.path.join(preds_dir, pred_data_fname)
 
+hyper_param_fpath = os.path.join(model_results_dir, hyperparam_fname)
+best_model_fpath = os.path.join(model_results_dir, best_model_fname)
+
 # append utilities directory to path
 for p in [utilities_dir, va_dir]:
     sys.path.append(p)
-    
+
+##########################
 #-- Cleaning Constants --#
+##########################
 
 # set base columns
-sub_cols = ['PassengerId', 
-            'Survived', 
-            'Pclass', 
-            'Age',
-            'SibSp', 
-            'Parch', 
-            'FamSize', 
-            'Fare', 
-            'Alone', 
-            'Mr', 
-            'Mrs', 
-            'Ms', 
-            'Priv', 
-            'Male',
-            'Embarked_Ord', 
-            'Dataset'
-            ]
+sub_cols = ['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'FamSize', 'Fare', 'Alone', 'Mr', 'Mrs', 'Ms', 'Priv', 'Male', 'Embarked_Ord', 'Dataset']
 
 # the title of a person indicates the person status
-title_map = {'Mr':'Mr', 
-             'Miss':'Ms', 
-             'Mrs':'Mrs', 
-             'Master':'Master',
-             'Dr':'Dr',
-             'Rev':'Rev',
-             'Col':'Col',
-             'Major':'Major',
-             'Ms':'Ms',
-             'Mlle':'Ms',
-             'Capt':'Capt',
-             'Don':'Mr',
-             'Jonkheer':'Jonkheer',
-             'Sir':'Sir',
-             'Lady':'Lady',
-             'Mme':'Mrs',
-             'Dona':'Mrs',
-             'theCountess':'Countess'
-             }
+title_map = {'Mr':'Mr', 'Miss':'Ms', 'Mrs':'Mrs', 'Master':'Master', 'Dr':'Dr', 'Rev':'Rev', 'Col':'Col', 'Major':'Major', 'Ms':'Ms', 'Mlle':'Ms', 'Capt':'Capt', 'Don':'Mr', 'Jonkheer':'Jonkheer', 'Sir':'Sir', 'Lady':'Lady', 'Mme':'Mrs', 'Dona':'Mrs', 'theCountess':'Countess'}
 
-    
 # create a map for the title values
-priv_map = {'Mr':'Mr', 
-            'Ms':'Ms', 
-            'Mrs':'Mrs', 
-            'Master':'Priv',
-            'Dr':'Priv',
-            'Rev':'Priv',
-            'Col':'Priv',
-            'Major':'Priv',
-            'Capt':'Priv',
-            'Jonkheer':'Priv',
-            'Sir':'Priv',
-            'Lady':'Priv',
-            'Countess':'Priv'
-            }
+priv_map = {'Mr':'Mr', 'Ms':'Ms', 'Mrs':'Mrs', 'Master':'Priv', 'Dr':'Priv', 'Rev':'Priv', 'Col':'Priv', 'Major':'Priv', 'Capt':'Priv', 'Jonkheer':'Priv', 'Sir':'Priv', 'Lady':'Priv', 'Countess':'Priv'}
 
 # create ordinal mapping for title
-title_ord_map = {'Mr':1,
-                 'Ms':2,
-                 'Mrs':3,
-                 'Priv':4
-                 }
+title_ord_map = {'Mr':1, 'Ms':2, 'Mrs':3, 'Priv':4}
 
 # create cabin map
-cab_map = {'A':'A', 'B':'B', 'BB':'B', 'BBB':'B', 'BBBB':'B', 'C':'C', 
-           'CC':'C', 'CCC':'C', 'D':'D', 'DD':'D', 'E':'E', 'EE':'E',
-           'F':'F', 'FG':'F', 'FE':'F', 'G':'G', 'T':'T'
-           }
+cab_map = {'A':'A', 'B':'B', 'BB':'B', 'BBB':'B', 'BBBB':'B', 'C':'C', 'CC':'C', 'CCC':'C', 'D':'D', 'DD':'D', 'E':'E', 'EE':'E', 'F':'F', 'FG':'F', 'FE':'F', 'G':'G', 'T':'T'}
 
 # create embarked map
 embarked_map = {'S':1, 'C':2, 'Q':3}
 
+#######################
+#-- Model Constants --#
+#######################
+
+# import relevant libraries
+
+# create empty dicitonaries to hold the age and survival models
+age_dict = {}  
+sur_dict = {}
+
+# set random state for models
+random_state = 123
+
+#-- Age --#
+
+# create the age models
+age_gbr_mod = ensemble.GradientBoostingRegressor(random_state = random_state) 
+age_rfr_mod = ensemble.RandomForestRegressor(random_state = random_state)
+
+# create age parameters
+age_gbr_params = {'loss':['lad'], 'learning_rate':[1.0, 0.9, 0.8], 'n_estimators':[50, 100], 'max_depth':[1, 3, 5], 'max_features':['sqrt', 'log2']}
+age_rfr_params = {'criterion':['mse', 'mae'], 'n_estimators':[100], 'min_samples_split':[2, 3], 'max_features':['auto', 'sqrt', 'log2'], 'n_jobs':[-1]}
+
+# create model and param dictionaries
+age_dict['gbr'] = {'model':age_gbr_mod, 'params':age_gbr_params}
+age_dict['rfr'] = {'model':age_rfr_mod, 'params':age_rfr_params}
+
+#-- Survival --#
+
+# create survival models
+sur_gbc_mod = ensemble.GradientBoostingClassifier(random_state = random_state)
+sur_rfc_mod = ensemble.RandomForestClassifier(random_state = random_state)
+sur_dtc_mod = tree.DecisionTreeClassifier(random_state = random_state)
+sur_abc_mod = ensemble.AdaBoostClassifier(base_estimator = sur_dtc_mod, random_state = random_state)
+sur_etc_mod = ensemble.ExtraTreesClassifier(random_state = random_state)
+sur_svc_mod = svm.SVC(random_state = random_state)
+#sur_knc_mod = neighbors.KNeighborsClassifier()
+#sur_lrc_mod = linear_model.LogisticRegression(random_state = random_state)
+#sur_nbc_mod = naive_bayes.GaussianNB()
+
+# create survival parameters
+sur_gbc_params = {'loss' : ["deviance"], 'n_estimators' : [100, 200, 300], 'learning_rate': [0.1, 0.05, 0.01], 'max_depth': [4, 8], 'min_samples_leaf': [100, 150], 'max_features': [0.3, 0.1]}
+sur_rfc_params ={"max_depth": [None], "max_features": [1, 3, 10],  "min_samples_split": [2, 3, 10], "min_samples_leaf": [1, 3, 10], "bootstrap": [False], "n_estimators" :[100, 300], "criterion": ["gini"]}
+sur_dtc_parmas = {'criterion':['gini', 'entropy'], 'splitter':['best', 'random'],'min_samples_split':[2, 3]}
+sur_abc_params = {"base_estimator__criterion" : ["gini", "entropy"], "base_estimator__splitter" :   ["best", "random"], "algorithm" : ["SAMME","SAMME.R"], "n_estimators" :[1,2], "learning_rate":  [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3,1.5]}
+sur_etc_params = {"max_depth": [None], "max_features": [1, 3, 10], "min_samples_split": [2, 3, 10], "min_samples_leaf": [1, 3, 10], "bootstrap": [False], "n_estimators" :[100,300], "criterion": ["gini"]}
+sur_svc_params = {'kernel': ['rbf'], 'gamma': [ 0.001, 0.01, 0.1, 1], 'C': [1, 10, 50, 100,200,300, 1000]}
+
+#sur_knc_params = {'n_neighbors':[3, 5, 7], 'weights':['uniform', 'distance'], 'algorithm':['auto'], 'p':[1, 2, 3], 'n_jobs':[-1]}
+#sur_lrc_params = {'penalty':['l1', 'l2', 'elasticnet', 'none'], 'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'], 'n_jobs':[-1]}
+#sur_nbc_params = {}
+
+# create model and param dictionaries
+sur_dict['gbc'] = {'model':sur_gbc_mod, 'params':sur_gbc_params}
+sur_dict['rfc'] = {'model':sur_rfc_mod, 'params':sur_rfc_params}
+sur_dict['dtc'] = {'model':sur_dtc_mod, 'params':sur_dtc_parmas}
+sur_dict['abc'] = {'model':sur_abc_mod, 'params':sur_abc_params}
+sur_dict['etc'] = {'model':sur_etc_mod, 'params':sur_etc_params}
+sur_dict['svc'] = {'model':sur_svc_mod, 'params':sur_svc_params}
+#sur_dict['knc'] = {'model':sur_knc_mod, 'params':sur_knc_params}
+#sur_dict['lrc'] = {'model':sur_lrc_mod, 'params':sur_lrc_params}
+#sur_dict['nbc'] = {'model':sur_nbc_mod, 'params':sur_nbc_params}
