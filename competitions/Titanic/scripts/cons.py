3febddbcfa44ac2d11f3e7ca605f9a26628877f3
oislen <oisin.leonard@gmail.com>
2021-02-12 15:36:35 +0000

    removed old models, updated title map and added lists on required columns

--- competitions/Titanic/scripts/cons.py
+++ competitions/Titanic/scripts/cons.py
@@ -81,11 +81,14 @@ for p in [utilities_dir, va_dir]:
 #-- Cleaning Constants --#
 ##########################
 
+# set id columns
+id_cols = ['PassengerId', 'Dataset', 'Survived']
+
 # set base columns
-sub_cols = ['PassengerId', 'Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'FamSize', 'Fare', 'Alone', 'Mr', 'Mrs', 'Ms', 'Priv', 'Male', 'Embarked_Ord', 'Dataset']
+drop_cols = ['Name', 'Sex', 'Ticket', 'Fare', 'Cabin', 'Embarked', 'Title', 'FamSize_Cat', 'Ticket_Prefix', 'Cabin_Letters']
 
 # the title of a person indicates the person status
-title_map = {'Mr':'Mr', 'Miss':'Ms', 'Mrs':'Mrs', 'Master':'Master', 'Dr':'Dr', 'Rev':'Rev', 'Col':'Col', 'Major':'Major', 'Ms':'Ms', 'Mlle':'Ms', 'Capt':'Capt', 'Don':'Mr', 'Jonkheer':'Jonkheer', 'Sir':'Sir', 'Lady':'Lady', 'Mme':'Mrs', 'Dona':'Mrs', 'theCountess':'Countess'}
+title_map = {'Mr':'Mr', 'Miss':'Ms', 'Mrs':'Mrs', 'Master':'Master', 'Dr':'Dr', 'Rev':'Rev', 'Col':'Col', 'Major':'Major', 'Ms':'Ms', 'Mlle':'Ms', 'Capt':'Capt', 'Don':'Mr', 'Jonkheer':'Jonkheer', 'Sir':'Sir', 'Lady':'Lady', 'Mme':'Mrs', 'Dona':'Mrs', 'Countess':'Countess'}
 
 # create a map for the title values
 priv_map = {'Mr':'Mr', 'Ms':'Ms', 'Mrs':'Mrs', 'Master':'Priv', 'Dr':'Priv', 'Rev':'Priv', 'Col':'Priv', 'Major':'Priv', 'Capt':'Priv', 'Jonkheer':'Priv', 'Sir':'Priv', 'Lady':'Priv', 'Countess':'Priv'}
@@ -102,8 +105,6 @@ embarked_map = {'S':1, 'C':2, 'Q':3}
 #######################
 #-- Model Constants --#
 #######################
-
-# import relevant libraries
 
 # create empty dicitonaries to hold the age and survival models
 age_dict = {}  
@@ -135,9 +136,6 @@ sur_dtc_mod = tree.DecisionTreeClassifier(random_state = random_state)
 sur_abc_mod = ensemble.AdaBoostClassifier(base_estimator = sur_dtc_mod, random_state = random_state)
 sur_etc_mod = ensemble.ExtraTreesClassifier(random_state = random_state)
 sur_svc_mod = svm.SVC(random_state = random_state)
-#sur_knc_mod = neighbors.KNeighborsClassifier()
-#sur_lrc_mod = linear_model.LogisticRegression(random_state = random_state)
-#sur_nbc_mod = naive_bayes.GaussianNB()
 
 # create survival parameters
 sur_gbc_params = {'loss' : ["deviance"], 'n_estimators' : [100, 200, 300], 'learning_rate': [0.1, 0.05, 0.01], 'max_depth': [4, 8], 'min_samples_leaf': [100, 150], 'max_features': [0.3, 0.1]}
@@ -147,10 +145,6 @@ sur_abc_params = {"base_estimator__criterion" : ["gini", "entropy"], "base_estim
 sur_etc_params = {"max_depth": [None], "max_features": [1, 3, 10], "min_samples_split": [2, 3, 10], "min_samples_leaf": [1, 3, 10], "bootstrap": [False], "n_estimators" :[100,300], "criterion": ["gini"]}
 sur_svc_params = {'kernel': ['rbf'], 'gamma': [ 0.001, 0.01, 0.1, 1], 'C': [1, 10, 50, 100,200,300, 1000]}
 
-#sur_knc_params = {'n_neighbors':[3, 5, 7], 'weights':['uniform', 'distance'], 'algorithm':['auto'], 'p':[1, 2, 3], 'n_jobs':[-1]}
-#sur_lrc_params = {'penalty':['l1', 'l2', 'elasticnet', 'none'], 'solver':['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'], 'n_jobs':[-1]}
-#sur_nbc_params = {}
-
 # create model and param dictionaries
 sur_dict['gbc'] = {'model':sur_gbc_mod, 'params':sur_gbc_params}
 sur_dict['rfc'] = {'model':sur_rfc_mod, 'params':sur_rfc_params}
@@ -158,6 +152,3 @@ sur_dict['dtc'] = {'model':sur_dtc_mod, 'params':sur_dtc_parmas}
 sur_dict['abc'] = {'model':sur_abc_mod, 'params':sur_abc_params}
 sur_dict['etc'] = {'model':sur_etc_mod, 'params':sur_etc_params}
 sur_dict['svc'] = {'model':sur_svc_mod, 'params':sur_svc_params}
-#sur_dict['knc'] = {'model':sur_knc_mod, 'params':sur_knc_params}
-#sur_dict['lrc'] = {'model':sur_lrc_mod, 'params':sur_lrc_params}
-#sur_dict['nbc'] = {'model':sur_nbc_mod, 'params':sur_nbc_params}
