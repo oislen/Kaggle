978cdc08eb653a2d80873168e2977001b15f463e
oislen <oisin.leonard@gmail.com>
2021-02-16 11:06:22 +0000

    moved model constants as function inputs

--- competitions/Titanic/scripts/preproc/clean_base_age.py
+++ competitions/Titanic/scripts/preproc/clean_base_age.py
@@ -8,9 +8,22 @@ Created on Sun Nov  4 19:18:53 2018
 # load in relevant libraries
 from graph.hist import hist
 from model.fit_age_mod import fit_age_mod
-import cons as cons
 
-def clean_base_age(base):
+def clean_base_age(base,
+                   y_col,
+                   X_col,
+                   model,
+                   params,
+                   random_state = 1234,
+                   train_size = 0.8,
+                   test_size = 0.2,
+                   random_split = True,
+                   scoring = 'neg_mean_squared_error',
+                   refit = True,
+                   verbose = 3,
+                   cv = 10,
+                   n_jobs = -1
+                   ):
         
     """
 
@@ -38,34 +51,15 @@ def clean_base_age(base):
 
     """
     
+    # split the training data on whether age is missing or not
+    base_train = base[base[y_col[0]].notnull()]
+    base_test = base[base[y_col[0]].isnull()]
+
     # plot age distribution before imputing
     hist(dataset = base,
-         num_var = ['Age'],
-         title = 'Histogram of Age - Pre Imputation'
+         num_var = y_col,
+         title = 'Histogram of {} - Pre Imputation'.format(y_col[0])
          )
-    
-    # split the training data on whether age is missing or not
-    base_train = base[base.Age.notnull()]
-    base_test = base[base.Age.isnull()]
-    
-    # extract out model and params
-    age_dict = cons.age_dict
-    
-    # set model constants
-    y_col = ['Age']
-    X_col =  ['Pclass', 'SibSp', 'Parch', 'FamSize', 'Fare', 'Alone', 'Mr', 'Mrs', 'Ms', 'Priv', 'Male', 'Embarked_Ord']
-    model = age_dict['rfr']['model']
-    params = age_dict['rfr']['params']
-    target_type = 'reg'
-    random_state = 123
-    train_size = 0.8
-    test_size = 0.2
-    random_split = True
-    scoring = 'neg_mean_squared_error'
-    refit = True
-    verbose = 3
-    cv = 10
-    n_jobs = -1
     
     # run age na fill model
     base_out = fit_age_mod(base_train = base_train,
@@ -74,7 +68,6 @@ def clean_base_age(base):
                            X_col = X_col,
                            model = model,
                            params = params,
-                           target_type = target_type,
                            random_state = random_state,
                            train_size = train_size,
                            test_size = test_size,
@@ -88,8 +81,8 @@ def clean_base_age(base):
 
     # plot age distribution after imputing
     hist(dataset = base_out,
-         num_var = ['Age'],
-         title = 'Histogram of Age - Post Imputation'
+         num_var = y_col,
+         title = 'Histogram of {} - Post Imputation'.format(y_col[0])
          )
     
     return base_out
