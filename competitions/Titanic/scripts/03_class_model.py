d2ad73adff39b581e9d265f1ee04d3984e06c8a6
oislen <oisin.leonard@gmail.com>
2021-02-17 12:51:29 +0000

    updated documentation. added input checks. added additional function input parameters

--- competitions/Titanic/scripts/03_class_model.py
+++ competitions/Titanic/scripts/03_class_model.py
@@ -12,7 +12,16 @@ import cons
 import os
 
 def class_model(base_engin_fpath,
-                pred_fpath
+                pred_fpath,
+                random_state = 123,
+                train_size = 0.8,
+                test_size = 0.3,
+                random_split = True,
+                scoring = 'accuracy',
+                cv = 10,
+                n_jobs = -1,
+                refit = True,
+                verbose = 0
                 ):
     
     """
@@ -21,18 +30,37 @@ def class_model(base_engin_fpath,
     
     Function Overview
     
-    This function trains a GBM model classifying the test data
+    This function iterates over the various sklearn models and trainings each one using the fit_sur_model() function.
+    
     
     Defaults
     
     class_model(base_engin_fpath,
-                pred_fpath
+                pred_fpath,
+                random_state = 123,
+                train_size = 0.8,
+                test_size = 0.3,
+                random_split = True,
+                scoring = 'accuracy',
+                cv = 10,
+                n_jobs = -1,
+                refit = True,
+                verbose = 0
                 )
     
     Parameters
     
     base_engin_fpath - String, the input file path for the base engineered data
     pred_fpath - String, the output file path for the GBM classifications
+    random_state - Integer, the random seed to set, default is 123
+    train_size - Float, the proportion of data to have in training set, default is 0.8
+    test_size - Float, the proportion of data to have in the testing set, default is 0.2
+    random_split - Boolean, whether to randomise the data before splitting, default is True
+    scoring - String, the type of scoring to perform on gbm model, default is 'accuracy'
+    cv - Integer, the number of folds to use for cross fold validation when training the model, default is 10
+    n_jobs - Integer, the number of cores to use when processing data, default is -1 for all cores
+    refit - Boolean, whether to refit the best model following grid search cross validation hypter parameter tuning, default is True
+    verbose - Integer, whether to print verbose updates when tuning model, default is 0
     
     Returns
     
@@ -41,15 +69,38 @@ def class_model(base_engin_fpath,
     Example
     
     class_model(base_engin_fpath = 'C:\\Users\\...\\base_engin.csv',
-                pred_fpath = 'C:\\Users\\...\\preds.csv'
+                pred_fpath = 'C:\\Users\\...\\preds.csv',
+                random_state = 123,
+                train_size = 0.8,
+                test_size = 0.3,
+                random_split = True,
+                scoring = 'accuracy',
+                cv = 10,
+                n_jobs = -1,
+                refit = True,
+                verbose = 0
                 )
         
     """
+    
+    print('Checking inputs ...')
+    
+    # check input data types
+    str_inputs = [base_engin_fpath, pred_fpath]
+    if any([type(val) != str for val in str_inputs]):
+        raise ValueError('Input params [base_engin_fpath, pred_fpath] must be str data types')
+    # check if input file path exists
+    if os.path.exists(base_engin_fpath) == False:
+        raise OSError('Input file path {} does not exist'.format(base_engin_fpath))
+    
+    print('Loading engineered feature data ...')
     
     # load in data
     base = pd.read_csv(base_engin_fpath, 
                        sep = cons.sep
                        )
+    
+    print('Splitting data into training and test sets ...')
     
     # split the data based on the original dataset
     base_train = base[base.Dataset == 'train']
@@ -63,26 +114,16 @@ def class_model(base_engin_fpath,
         
         print('~~~~~ Working on model: {} ...'.format(model_name))
         
-        # set model constants
-        #model_name = 'rfc'
+        # set model constants gien model name
         y_col = cons.y_col
         X_col =  base.columns.drop(cons.id_cols).tolist()
         model = sur_dict[model_name]['model']
         params = sur_dict[model_name]['params']
-        target_type = 'class'
-        random_state = 123
-        train_size = 0.8
-        test_size = 0.2
-        random_split = True
-        scoring = 'accuracy'
-        refit = True
-        verbose = 3
-        cv = 10
-        n_jobs = -1
-        
         report_dir = cons.model_results_dir.format(model_name)
         
+        # check if model report directory exists
         if os.path.exists(report_dir) == False:
+            # if not, create it
             os.makedirs(report_dir)
         
         # fit model
@@ -93,7 +134,6 @@ def class_model(base_engin_fpath,
                                 model_name = model_name,
                                 model = model,
                                 params = params,
-                                target_type = target_type,
                                 random_state = random_state,
                                 train_size = train_size,
                                 test_size = test_size,
@@ -129,7 +169,27 @@ if __name__ == '__main__':
     base_engin_fpath = cons.base_engin_data_fpath
     pred_fpath = cons.pred_data_fpath
     
+    # extract out model constants
+    random_state = cons.random_state
+    train_size = cons.train_size
+    test_size = cons.test_size
+    random_split = cons.random_split
+    scoring = 'accuracy'
+    refit = cons.refit
+    verbose = cons.verbose
+    cv = cons.cv
+    n_jobs = cons.n_jobs
+
     # run calssification model
     class_model(base_engin_fpath = base_engin_fpath,
-                pred_fpath = pred_fpath
+                pred_fpath = pred_fpath,
+                random_state = random_state,
+                train_size = train_size,
+                test_size = test_size,
+                random_split = random_split,
+                scoring = scoring,
+                refit = refit,
+                verbose = verbose,
+                cv = cv,
+                n_jobs = n_jobs
                 )
