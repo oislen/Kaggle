82435edf68398900d4ac78ff48dc2886cd7ed116
oislen <oisin.leonard@gmail.com>
2021-02-18 11:16:26 +0000

    added doc string template, input parameter checks, moved function constants to cons.py and added __main__ call at bottom of script

--- competitions/HousePrices/scripts/02_create_base.py
+++ competitions/HousePrices/scripts/02_create_base.py
@@ -5,73 +5,96 @@ Created on Sun Nov 18 12:23:52 2018
 @author: oislen
 """
 
-"""
-#####################
-#-- Preliminaries --#
-#####################
-"""
-
-print('Loading in libraries and data ...')
-
 # load in libraries
+import os
 import pandas as pd
 import numpy as np
+import cons 
 
-# load in data
-input_dir = 'C:\\Users\\User\\Documents\\GitHub\\Kaggle\\HousePrices\\data\\'
-train_name = 'train.csv'
-test_name = 'test.csv'
-train = pd.read_csv(input_dir + train_name, sep = ',')
-test = pd.read_csv(input_dir + test_name, sep = ',')
+def create_base(train_data_fpath,
+                test_data_fpath,
+                base_data_fpath
+                ):
+    
+    """
+    
+    Create Base Data Documentation
+    
+    Function Overview
+    
+    Defaults
+    
+    Parameters
+    
+    Returns
+    
+    Example
+    
+    """
+    
+    print('Checking inputs ...')
+    
+    # check the string input parameter
+    str_params = [train_data_fpath, test_data_fpath, base_data_fpath]
+    if any([type(param) != str for param in str_params]):
+        raise TypeError('Input parameters [train_data_fpath, test_data_fpath, base_data_fpath] must be string data types.')
+    # check training file exists
+    if os.path.exists(train_data_fpath) == False:
+        raise OSError('Input train file path {} does not exist'.format(train_data_fpath))
+    # check test file exists
+    if os.path.exists(test_data_fpath) == False:
+        raise OSError('Input test file path {} does not exist.'.format(test_data_fpath))
+        
+    print('Loading train and test data ...')
+    
+    # load the train and test data
+    train_data = pd.read_csv(train_data_fpath, sep = cons.sep)
+    test_data = pd.read_csv(test_data_fpath, sep = cons.sep)
+    
+    print('Concatenating files ...')
+    
+    # create a 'SalePrice' column in test
+    test_data['SalePrice'] = np.nan
+    
+    # create a dataset indicator column
+    train_data['Dataset'] = 'train'
+    test_data['Dataset'] = 'test'
+    
+    # create the list of concate objects
+    cat_obs = [train_data, test_data]
+    
+    # row bind the datasets
+    base_data = pd.concat(objs = cat_obs, 
+                          axis = 0, 
+                          sort = False, 
+                          ignore_index = True
+                          )
+    
+    # create logSalePrice (Will be target)
+    base_data['logSalePrice'] = np.log1p(base_data['SalePrice'])
+    
+    print('Outputting base file ...')
+    
+    # output the dataset
+    base_data.to_csv(base_data_fpath,
+                     sep = cons.sep,
+                     encoding = cons.encoding,
+                     header = cons.header,
+                     index = cons.index
+                     )
+    
+    return 0
 
-
-"""
-#########################
-#-- Concatenate Files --#
-#########################
-"""
-
-print('Concatenating files ...')
-
-# note the training set as an extra column, the target survived
-train.columns
-test.columns
-
-# create a 'SalePrice' column in test
-test['SalePrice'] = np.nan
-
-# create a dataset indicator column
-train['Dataset'] = 'train'
-test['Dataset'] = 'test'
-
-# row bind the datasets
-base = pd.concat(objs = [train, test], axis = 0, sort = False)
-
-"""
-###########################
-#-- Feature Engineering --#
-###########################
-"""
-
-# create logSalePrice (Will be target)
-base['logSalePrice'] = np.log1p(base['SalePrice'])
-
-"""
-########################
-#-- Output Base File --#
-########################
-"""
-
-print('Outputting base file ...')
-
-# define the output location and filename
-output_filename = 'base.csv'
-
-# output the dataset
-base.to_csv(input_dir + output_filename,
-               sep = '|',
-               encoding = 'utf-8',
-               header = True,
-               index = False
-               )
-
+# if running script as main programme
+if __name__ == '__main__':
+    
+    # extract file paths from cons
+    train_data_fpath = cons.train_data_fpath
+    test_data_fpath = cons.test_data_fpath
+    base_data_fpath = cons.base_data_fpath
+    
+    # call function
+    create_base(train_data_fpath = train_data_fpath,
+                test_data_fpath = test_data_fpath,
+                base_data_fpath = base_data_fpath
+                )
