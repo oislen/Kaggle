94e4b2ab286572e5a7d56ba5615a56b38aeae056
oislen <oisin.leonard@gmail.com>
2021-02-18 11:15:17 +0000

    added .csv file constants, and added train, test and base file names and paths to cons.py.

--- competitions/HousePrices/scripts/cons.py
+++ competitions/HousePrices/scripts/cons.py
@@ -15,13 +15,29 @@ download_data = True
 unzip_data = True
 del_zip = True
 
+# set .csv constants
+sep = ','
+encoding = 'utf-8'
+header = True
+index = False
+
 # set directories
 git_dir = 'C:\\Users\\User\\Documents\\GitHub'
 root_dir = os.path.join(git_dir, 'Kaggle')
 utilities_dir = os.path.join(root_dir, 'utilities')
-titanic_comp_dir = os.path.join(root_dir, 'competitions\\HousePrices')
-scripts_dir = os.path.join(titanic_comp_dir, 'scripts')
-data_dir = os.path.join(titanic_comp_dir, 'data')
+houseprices_comp_dir = os.path.join(root_dir, 'competitions\\HousePrices')
+scripts_dir = os.path.join(houseprices_comp_dir, 'scripts')
+data_dir = os.path.join(houseprices_comp_dir, 'data')
+
+# create file names
+train_data_fname = 'train.csv'
+test_data_fname = 'test.csv'
+base_data_fname = 'base.csv'
+
+# create file paths
+train_data_fpath = os.path.join(data_dir, train_data_fname)
+test_data_fpath = os.path.join(data_dir, test_data_fname)
+base_data_fpath = os.path.join(data_dir, base_data_fname)
 
 # append utilities directory to path
 for p in [utilities_dir]:
