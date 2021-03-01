8315ebbc93c4433091908f05961f815d307e7475
oislen <oisin.leonard@gmail.com>
2021-03-01 10:34:14 +0000

    parameterised various stages of engineering script and added standardisation from stacked model script

--- competitions/HousePrices/scripts/preproc/engin_base_data.py
+++ competitions/HousePrices/scripts/preproc/engin_base_data.py
@@ -5,16 +5,22 @@ Created on Thu Feb 18 16:51:39 2021
 @author: oislen
 """
 
+# import relevant libraries
 import pandas as pd
 import cons
 from scipy.stats import skew
 from preproc.derive_variables import derive_variables
 from preproc.tree_feat_imp import tree_feat_imp
 from preproc.boxcox_trans import boxcox_trans
+from preproc.standardise_variables import standardise_variables
 
 def engin_base_data(clean_data_fpath,
                     engin_data_fpath,
-                    top_n_int_terms = 25
+                    top_n_int_terms = 25,
+                    interterms = True,
+                    feat_sel = True,
+                    boxcox = True,
+                    stand = True
                     ):
     
     """
@@ -54,86 +60,109 @@ def engin_base_data(clean_data_fpath,
     # drop SalePrice
     clean = clean.drop(columns = 'SalePrice')
     
-    print('Deriving interaction terms ...')
+    # if derviving interaction terms
+    if interterms == True:
     
-    # extract out the base columns
-    clean_cols = clean.columns.tolist()
-    
-    # extract out the integer columns
-    attr_cols = [col for col in clean_cols if col not in cons.tar_cols]
-    
-    # create interaction terms
-    int_data = derive_variables(dataset = clean,
-                                attr = attr_cols,
-                                var_type = 'interaction'
-                                )
-    
-    # create the concatenation object list
-    concat_objs = [clean[clean_cols], int_data]
-    
-    # create the engineered data by concatenating the base data with the interaction data
-    engin = pd.concat(objs = concat_objs, axis = 1)
-
-    print('Performing feature importance on all terms ...')
-    
-    # create a list of all predictors
-    pred_cols = attr_cols + int_data.columns.tolist()
-    
-    # extract training data
-    y_train = engin.loc[engin['Dataset'] == 'train', cons.y_col[0]]
-    X_train = engin.loc[engin['Dataset'] == 'train', pred_cols]
-    
-    # create a tree model
-    model = cons.rfr_mod
-    
-    # determine the feature importance
-    feat_imp = tree_feat_imp(model = model,
-                             y_train = y_train,
-                             X_train = X_train
-                             )
-    
-    # consider only interaction terms
-    int_feat_imp_filt = pd.Series(feat_imp.index).str.contains('_x_').tolist()
-    
-    # filter out non interaction terms from feat_imp
-    feat_imp_sub = feat_imp.loc[int_feat_imp_filt, :].reset_index()
-    
-    # extract out the important features 
-    top_int_feat = feat_imp_sub['Predictor'].head(top_n_int_terms).tolist()
-    
-    # add in additional variables to enable the interaction effects
-    out_vars = clean_cols + top_int_feat
-    
-    # create the final dataset
-    final_data = engin[out_vars]
-    
-    print('Transforming skewed attributes ...')
-    
-    # create a list of the numeric columns
-    num_cols = final_data.columns[(final_data.dtypes == 'int64') | (final_data.dtypes == 'float64')]
-    num_cols = num_cols.drop(['Id', 'logSalePrice'])
-    
-    # remove columns with less than 20 values
-    high_card_num_cols_filt = final_data[num_cols].apply(lambda x: x.nunique(), axis = 0) > 25
-    high_card_num_cols = final_data[num_cols].columns[high_card_num_cols_filt]
-    
-    # determine highly skewed data
-    skewed_feats = final_data[high_card_num_cols].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
-    skewness = pd.DataFrame({'Skew' :skewed_feats})
-    
-    # drop unneccessary columns
-    skewness = skewness[abs(skewness) > 0.75].dropna()
-    bin_cols = skewness.index.astype(str).str.extract(pat = '(^\w+_bin$)', expand = False).dropna().tolist()
-    skewness = skewness.drop(index = bin_cols)
-    skewed_cols = skewness.index.tolist()
+        print('Deriving interaction terms ...')
         
-    # run boxcox power transformation to remove attribute skew
-    boxcox_data = boxcox_trans(dataset = final_data,
-                               attr = skewed_cols
-                               )    
+        # extract out the base columns
+        clean_cols = clean.columns.tolist()
+        
+        # extract out the integer columns
+        attr_cols = [col for col in clean_cols if col not in cons.tar_cols]
+        
+        # create interaction terms
+        int_data = derive_variables(dataset = clean,
+                                    attr = attr_cols,
+                                    var_type = 'interaction'
+                                    )
+        
+        # create the concatenation object list
+        concat_objs = [clean[clean_cols], int_data]
+        
+        # create the engineered data by concatenating the base data with the interaction data
+        engin = pd.concat(objs = concat_objs, axis = 1)
+
+    # if running feature selection
+    if feat_sel == True:
+            
+        print('Performing feature importance on all terms ...')
+        
+        # create a list of all predictors
+        pred_cols = attr_cols + int_data.columns.tolist()
+        
+        # extract training data
+        y_train = engin.loc[engin['Dataset'] == 'train', cons.y_col[0]]
+        X_train = engin.loc[engin['Dataset'] == 'train', pred_cols]
+        
+        # create a tree model
+        model = cons.rfr_mod
+        
+        # determine the feature importance
+        feat_imp = tree_feat_imp(model = model,
+                                 y_train = y_train,
+                                 X_train = X_train
+                                 )
+        
+        # consider only interaction terms
+        int_feat_imp_filt = pd.Series(feat_imp.index).str.contains('_x_').tolist()
+        
+        # filter out non interaction terms from feat_imp
+        feat_imp_sub = feat_imp.loc[int_feat_imp_filt, :].reset_index()
+        
+        # extract out the important features 
+        top_int_feat = feat_imp_sub['Predictor'].head(top_n_int_terms).tolist()
+        
+        # add in additional variables to enable the interaction effects
+        out_vars = clean_cols + top_int_feat
+        
+        # create the final dataset
+        final_data = engin[out_vars]
     
-    # update the columns
-    final_data[skewed_cols] = boxcox_data[skewed_cols]
+    # if running boxcox transformation
+    if boxcox == True:
+    
+        print('Transforming skewed attributes ...')
+    
+        # create a list of the numeric columns
+        num_cols = final_data.columns[(final_data.dtypes == 'int64') | (final_data.dtypes == 'float64')]
+        num_cols = num_cols.drop(['Id', 'logSalePrice'])
+        
+        # remove columns with less than 20 values
+        high_card_num_cols_filt = final_data[num_cols].apply(lambda x: x.nunique(), axis = 0) > 25
+        high_card_num_cols = final_data[num_cols].columns[high_card_num_cols_filt]
+        
+        # determine highly skewed data
+        skewed_feats = final_data[high_card_num_cols].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skewness = pd.DataFrame({'Skew' :skewed_feats})
+        
+        # drop unneccessary columns
+        skewness = skewness[abs(skewness) > 0.75].dropna()
+        bin_cols = skewness.index.astype(str).str.extract(pat = '(^\w+_bin$)', expand = False).dropna().tolist()
+        skewness = skewness.drop(index = bin_cols)
+        skewed_cols = skewness.index.tolist()
+            
+        # run boxcox power transformation to remove attribute skew
+        boxcox_data = boxcox_trans(dataset = final_data,
+                                   attr = skewed_cols
+                                   )    
+        
+        # update the columns
+        final_data[skewed_cols] = boxcox_data[skewed_cols]
+    
+    # if standardising variables
+    if stand == True:
+        
+        print('Standardising data ...')
+        
+        # create a list of variables to standardise
+        stand_cols = clean.columns.drop(cons.ref_cols).tolist()
+        
+        # standardise the dataset using the robust scalar method
+        final_data[stand_cols] = standardise_variables(dataset = clean,
+                                                       attr = stand_cols,
+                                                       stand_type = 'robust'
+                                                       )
         
     print('Outputting engineered data ...')
     
