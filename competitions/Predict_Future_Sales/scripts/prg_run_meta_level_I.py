c3da7f449374d0beaa2287d0af9df006ca825e2b
oislen <oisin.leonard@gmail.com>
2021-05-16 11:02:49 +0100

    added relevant parameter inputs to constants file and removed corresponding parameter inputs from various function calls

--- competitions/Predict_Future_Sales/scripts/prg_run_meta_level_I.py
+++ competitions/Predict_Future_Sales/scripts/prg_run_meta_level_I.py
@@ -6,7 +6,6 @@ Created on Sun May 17 10:57:04 2020
 """
 
 # import relevant libraries
-import cons
 from exe_model import exe_model
 from reference.gen_feature_selection import gen_feature_selection
 
@@ -14,7 +13,7 @@ from reference.gen_feature_selection import gen_feature_selection
 skip_train = False # whether to skip training and make predictions with pre-fitted models
 n = 30 # number of top ranked feature importance attributes to consdier
 start = 2 # which step to start from
-end = 2 # which step to end at
+end = 5 # which step to end at
 date = '20200523' # set the date to output the files with
 #date = dt.datetime.today().strftime('%Y%m%d') # alternatively use todays date
 
@@ -22,37 +21,31 @@ date = '20200523' # set the date to output the files with
 run_range = range(start, end + 1)
 
 if 1 in run_range:
-    
     print('~~~~~ Generating feature importance ...')
-    
     # run feature importance
-    gen_feature_selection(cons,  feat_type = 'randforest', n_cpu = cons.n_cpu) 
-    gen_feature_selection(cons,  feat_type = 'gradboost', n_cpu = cons.n_cpu) 
+    gen_feature_selection(feat_type = 'randforest') 
+    gen_feature_selection(feat_type = 'gradboost') 
     
 if 2 in run_range:
     print('~~~~~ Working on Decision Tree Models ...')
     # execute the model
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 3, rand_state = 1, model_type = 'dtree', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 5, rand_state = 2, model_type = 'dtree', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 7, rand_state = 3, model_type = 'dtree', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 3, rand_state = 1, model_type = 'dtree', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 5, rand_state = 2, model_type = 'dtree', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 7, rand_state = 3, model_type = 'dtree', n = n, skip_train = skip_train, date = date)
 
-if 3 in run_range:
-    print('~~~~~ Working on KNN Models ...')
-    exe_model(cons = cons, feat_imp = 'randforest', rand_state = 1, model_type = 'knn', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
+#if 3 in run_range:
+#    print('~~~~~ Working on KNN Models ...')
+#    exe_model(feat_imp = 'randforest', rand_state = 1, model_type = 'knn', n = n, skip_train = skip_train, date = date)
     
 if 4 in run_range:
-    
     print('~~~~~ Working on Random Forest Models ...')
-    
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 3, rand_state = 1, model_type = 'randforest', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 5, rand_state = 2, model_type = 'randforest', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'randforest', max_dept = 7, rand_state = 3, model_type = 'randforest', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 3, rand_state = 1, model_type = 'randforest', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 5, rand_state = 2, model_type = 'randforest', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'randforest', max_dept = 7, rand_state = 3, model_type = 'randforest', n = n, skip_train = skip_train, date = date)
     
 if 5 in run_range:
-    
     print('~~~~~ Working on Gradient Boosting Models ...')
-    
-    exe_model(cons = cons, feat_imp = 'gradboost', max_dept = 3, rand_state = 1, model_type = 'gradboost', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'gradboost', max_dept = 5, rand_state = 2, model_type = 'gradboost', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
-    exe_model(cons = cons, feat_imp = 'gradboost', max_dept = 7, rand_state = 3, model_type = 'gradboost', n = n, skip_train = skip_train, n_cpu = cons.n_cpu, date = date)
+    exe_model(feat_imp = 'gradboost', max_dept = 3, rand_state = 1, model_type = 'gradboost', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'gradboost', max_dept = 5, rand_state = 2, model_type = 'gradboost', n = n, skip_train = skip_train, date = date)
+    exe_model(feat_imp = 'gradboost', max_dept = 7, rand_state = 3, model_type = 'gradboost', n = n, skip_train = skip_train, date = date)
     
