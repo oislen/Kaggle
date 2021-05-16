000d0cfdd5a4069e27f151be3a21b06bff823d40
oislen <oisin.leonard@gmail.com>
2021-05-16 11:00:52 +0100

    copied automated kaggle submission script from house prices competition

---
+++ competitions/Predict_Future_Sales/scripts/submit_future_sales_preds.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 10:55:21 2021
+
+@author: oislen
+"""
+
+# load relevant libraries
+import cons
+from sub_comp_preds import sub_comp_preds
+from get_comp_subs import get_comp_subs
+
+# submit model predictions
+sub_comp_preds(comp_name = cons.comp_name,
+               pred_data_fpath = cons.preds_data_fpath,
+               sub_mess = "testing kaggle api"
+               )
+
+# get submission results
+get_comp_subs(comp_name = cons.comp_name)
