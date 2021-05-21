8ffb60832dcbf374b6d87e0089e7ef62e56d6dcf
oislen <oisin.leonard@gmail.com>
2021-05-21 09:57:23 +0100

    removed date parameter

--- competitions/Predict_Future_Sales/scripts/prg_run_meta_level_I.py
+++ competitions/Predict_Future_Sales/scripts/prg_run_meta_level_I.py
@@ -10,11 +10,11 @@ import cons
 from exe_model import exe_model
 from reference.gen_feature_selection import gen_feature_selection
 
-# user settings
-skip_train = False # whether to skip training and make predictions with pre-fitted models
+# user True
+skip_train = True # whether to skip training and make predictions with pre-fitted models
 n = 30 # number of top ranked feature importance attributes to consdier
 start = 2 # which step to start from
-end = 2 # which step to end at
+end = 5 # which step to end at
 
 # generate the run range
 run_range = range(start, end + 1)
@@ -28,17 +28,17 @@ if 1 in run_range:
 if 2 in run_range:
     print('~~~~~ Working on Decision Tree Models ...')
     # execute the model
-    exe_model(feat_imp = 'randforest', model_type = 'dtree', n = n, skip_train = skip_train, date = cons.date)
+    exe_model(feat_imp = 'randforest', model_type = 'dtree', n = n, skip_train = skip_train)
 
 #if 3 in run_range:
 #    print('~~~~~ Working on KNN Models ...')
-#    exe_model(feat_imp = 'randforest', model_type = 'knn', n = n, skip_train = skip_train, date = date)
+#    exe_model(feat_imp = 'randforest', model_type = 'knn', n = n, skip_train = skip_train)
     
 if 4 in run_range:
     print('~~~~~ Working on Random Forest Models ...')
-    exe_model(feat_imp = 'randforest', model_type = 'randforest', n = n, skip_train = skip_train, date = cons.date)
+    exe_model(feat_imp = 'randforest', model_type = 'randforest', n = n, skip_train = skip_train)
     
 if 5 in run_range:
     print('~~~~~ Working on Gradient Boosting Models ...')
-    exe_model(feat_imp = 'gradboost', model_type = 'gradboost', n = n, skip_train = skip_train, date = cons.date)
+    exe_model(feat_imp = 'gradboost', model_type = 'gradboost', n = n, skip_train = skip_train)
     
