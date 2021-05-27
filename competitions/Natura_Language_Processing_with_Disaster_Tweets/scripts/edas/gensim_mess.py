f89c4ffb3a7b89936151e71a3858d5f9891a971d
oislen <oisin.leonard@gmail.com>
2021-05-27 20:23:38 +0100

    added section on keras model logic

--- competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/edas/gensim_mess.py
+++ competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/edas/gensim_mess.py
@@ -23,6 +23,17 @@ from normalise_tweet import normalise_tweet
 from lda_topic_prob import lda_topic_prob
 from topic_df import topic_df
 
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from tqdm import tqdm
+from keras.models import Sequential
+from keras.layers import Embedding,LSTM,Dense,SpatialDropout1D
+from keras.initializers import Constant
+from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
+from nltk.tokenize import word_tokenize
+from tqdm import tqdm
+
 #################
 #-- Data Prep --#
 #################
@@ -94,9 +105,9 @@ lda_topic_df = topic_df(data = data, prob_col = 'lda_topic_prob', text_col = 'te
 ###########################
 
 # create a stram of sentances for word 2 vec
-sent_stream = [sent.split(' ') for sent in data['text_clean_bigram'].tolist()]
+sent_stream = [sent.split(' ') for sent in data['text_norm_clean'].tolist()]
 # initiate the model and perform the first epoch of training
-tweet2vec = Word2Vec(sent_stream, vector_size=100, window=5, min_count=20, sg=1, workers=2, epochs = 5, negative = 10)
+tweet2vec = Word2Vec(sent_stream, vector_size = 100, window = 3, min_count = 1, sg = 1)
 
 # check word vectors for storm
 tweet2vec.wv['storm']
@@ -127,3 +138,48 @@ tsne_vectors = tsne.fit_transform(word_vectors.values)
 tsne_vectors_df = pd.DataFrame(tsne_vectors, index = word_vectors.index, columns = [u'x_coord', u'y_coord'])
 # plot results as a scatterplot
 tsne_vectors_df.plot.scatter(x = 'x_coord', y = 'y_coord')
+
+###################
+#-- Keras Model --#
+###################
+
+
+# create training data
+# note bi-grams negatively affected in word2vec
+# create corpus object
+embedding_dict = word_vecs_dict
+corpus = [word for sent in data['text_norm_clean'].to_list() for word in sent.split() if word in word_vecs_dict.keys()]
+MAX_LEN = 50
+tokenizer_obj = Tokenizer()
+tokenizer_obj.fit_on_texts(corpus)
+sequences = tokenizer_obj.texts_to_sequences(corpus)
+tweet_pad = pad_sequences(sequences,maxlen = MAX_LEN, truncating = 'post', padding = 'post')
+word_index = tokenizer_obj.word_index
+num_words=len(word_index)+1
+embedding_matrix=np.zeros((num_words,100))
+for word,i in tqdm(word_index.items()):
+    if i > num_words:
+        continue
+    emb_vec=embedding_dict.get(word)
+    if emb_vec is not None:
+        embedding_matrix[i]=emb_vec
+
+# fit model
+model=Sequential()
+embedding=Embedding(num_words,100,embeddings_initializer=Constant(embedding_matrix),input_length=MAX_LEN,trainable=False)
+model.add(embedding)
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+optimzer=Adam(learning_rate=1e-5)
+model.compile(loss='binary_crossentropy',optimizer=optimzer,metrics=['accuracy'])
+model.summary()
+train_df=tweet_pad[:train.shape[0]]
+test_df=tweet_pad[train.shape[0]:]
+X_train,X_test,y_train,y_test=train_test_split(train_df,train['target'].values,test_size=0.15)
+history=model.fit(X_train,y_train,batch_size=4,epochs=15,validation_data=(X_test,y_test),verbose=2)
+
+# make test predictions
+y_pre=model.predict(test)
+y_pre=np.round(y_pre).astype(int).reshape(3263)
+sub=pd.DataFrame({'id':sample_submission['id'].values.tolist(),'target':y_pre})
