aca06a3325503f7b6bbd30e3d9694151b2dde955
oislen <oisin.leonard@gmail.com>
2021-05-28 11:54:24 +0100

    added glove wiki and tweet file locations and competition prediction file locations

--- competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/cons.py
+++ competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/cons.py
@@ -23,7 +23,7 @@ del_zip = True
 git_dir = 'C:\\Users\\User\\Documents\\GitHub'
 #git_dir = '/run'
 root_dir = os.path.join(git_dir, 'Kaggle')
-comp_dir = os.path.join(root_dir, 'competitions\\Natura_Language_Processing_with_Disaster_Tweets')
+comp_dir = os.path.join(root_dir, 'competitions\\Disaster_Tweets')
 data_dir = os.path.join(comp_dir, 'data')
 scripts_dir = os.path.join(comp_dir, 'scripts')
 reports_dir = os.path.join(comp_dir, 'report')
@@ -47,14 +47,43 @@ raw_train_fpath = os.path.join(raw_data_dir, raw_train_fname)
 raw_test_fpath = os.path.join(raw_data_dir, raw_test_fname)
 raw_sample_submission_fpath = os.path.join(raw_data_dir, raw_sampe_submissoin_fname)
 
+# set the model predictions output file locaiton and name
+pred_data_dir = os.path.join(data_dir, 'pred')
+pred_fname = 'keras_rnn_preds.csv'
+pred_fpath = os.path.join(pred_data_dir, pred_fname)
+
 # append utilities directory to path
 for p in [utilities_comp, utilities_graph, utilities_model, utilties_preproc, utilities_nlp]:
     sys.path.append(p)
 
+# set url for glove wikipedia pretrained word vectors
+glove_wiki_zip_url = 'http://nlp.stanford.edu/data/glove.6B.zip'
+glove_wiki_zip_fname = glove_wiki_zip_url.split('/')[-1]
+glove_wiki_zip_fpath = os.path.join(ref_data_dir, glove_wiki_zip_fname)
+# set the names of the glove text files
+glove_50d_fname = 'glove.6B.50d.txt'
+glove_100d_fname = 'glove.6B.100d.txt'
+glove_200d_fname = 'glove.6B.200d.txt'
+glove_300d_fname = 'glove.6B.300d.txt'
+# set the file paths of the glove text files
+glove_50d_fpath = os.path.join(ref_data_dir, glove_50d_fname)
+glove_100d_fpath = os.path.join(ref_data_dir, glove_100d_fname)
+glove_200d_fpath = os.path.join(ref_data_dir, glove_200d_fname)
+glove_300d_fpath = os.path.join(ref_data_dir, glove_300d_fname)
 # set url for glove twitter pretrained word vectors
-glove_twitter_url = 'http://nlp.stanford.edu/data/glove.twitter.27B.zip'
-glove_twitter_fname = glove_twitter_url.split('/')[-1]
-glove_twitter_fpath = os.path.join(ref_data_dir, glove_twitter_fname)
+glove_twitter_zip_url = 'http://nlp.stanford.edu/data/glove.twitter.27B.zip'
+glove_twitter_zip_fname = glove_twitter_zip_url.split('/')[-1]
+glove_twitter_zip_fpath = os.path.join(ref_data_dir, glove_twitter_zip_fname)
+# set the names of the glove text files
+glove_twitter_25d_fname = 'glove.twitter.27B.25d.txt'
+glove_twitter_50d_fname = 'glove.twitter.27B.50d.txt'
+glove_twitter_100d_fname = 'glove.twitter.27B.100d.txt'
+glove_twitter_200d_fname = 'glove.twitter.27B.200d.txt'
+# set the file paths of the glove text files
+glove_twitter_25d_fpath = os.path.join(ref_data_dir, glove_twitter_25d_fname)
+glove_twitter_50d_fpath = os.path.join(ref_data_dir, glove_twitter_50d_fname)
+glove_twitter_100d_fpath = os.path.join(ref_data_dir, glove_twitter_100d_fname)
+glove_twitter_200d_fpath = os.path.join(ref_data_dir, glove_twitter_200d_fname)
 
 ##########################
 #-- Cleaning Constants --#
