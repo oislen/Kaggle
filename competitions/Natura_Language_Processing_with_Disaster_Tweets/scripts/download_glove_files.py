db74af4a00f2e2cb92a3f14ad5ac5a52ba492809
oislen <oisin.leonard@gmail.com>
2021-05-28 11:55:06 +0100

    added glove wiki files url to executable script

--- competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/download_glove_files.py
+++ competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/download_glove_files.py
@@ -10,6 +10,6 @@ import cons
 from download_url_files import download_url_files
 
 # download glove files
-download_url_files(url = cons.glove_twitter_url, 
-                   out_fpath = cons.glove_twitter_fpath
+download_url_files(url = cons.glove_wiki_zip_url, 
+                   out_fpath = cons.glove_wiki_zip_fpath
                    )
