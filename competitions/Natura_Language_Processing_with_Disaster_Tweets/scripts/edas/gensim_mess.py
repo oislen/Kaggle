e9877edf2244312a3719369686952a3617298e05
oislen <oisin.leonard@gmail.com>
2021-05-28 11:55:36 +0100

    removed keras model code from gensim eda script

--- competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/edas/gensim_mess.py
+++ competitions/Natura_Language_Processing_with_Disaster_Tweets/scripts/edas/gensim_mess.py
@@ -12,7 +12,6 @@ sys.path.append(os.path.dirname(os.getcwd()))
 import cons
 import re
 import spacy
-import pandas as pd
 import numpy as np
 from gensim.models import Phrases
 from gensim.corpora import Dictionary
@@ -22,17 +21,8 @@ from sklearn.manifold import TSNE
 from normalise_tweet import normalise_tweet
 from lda_topic_prob import lda_topic_prob
 from topic_df import topic_df
-
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from tqdm import tqdm
-from keras.models import Sequential
-from keras.layers import Embedding,LSTM,Dense,SpatialDropout1D
-from keras.initializers import Constant
-from sklearn.model_selection import train_test_split
-from keras.optimizers import Adam
-from nltk.tokenize import word_tokenize
-from tqdm import tqdm
+from token_dist_check import token_dist_check
+import pandas as pd
 
 #################
 #-- Data Prep --#
@@ -56,6 +46,7 @@ data['text_norm'] = data['text'].apply(lambda x: normalise_tweet(tweet = x, nlp 
 # remove special @ characters
 # could replace cases with @user?
 data['text_norm_clean'] = data['text_norm'].apply(lambda x: re.sub('@', '', x))
+token_dist_check(data = data, text_col = 'text_norm_clean')
 
 ################################
 #-- Bi-gram Phrase Modelling --#
@@ -139,47 +130,4 @@ tsne_vectors_df = pd.DataFrame(tsne_vectors, index = word_vectors.index, columns
 # plot results as a scatterplot
 tsne_vectors_df.plot.scatter(x = 'x_coord', y = 'y_coord')
 
-###################
-#-- Keras Model --#
-###################
 
-
-# create training data
-# note bi-grams negatively affected in word2vec
-# create corpus object
-embedding_dict = word_vecs_dict
-corpus = [word for sent in data['text_norm_clean'].to_list() for word in sent.split() if word in word_vecs_dict.keys()]
-MAX_LEN = 50
-tokenizer_obj = Tokenizer()
-tokenizer_obj.fit_on_texts(corpus)
-sequences = tokenizer_obj.texts_to_sequences(corpus)
-tweet_pad = pad_sequences(sequences,maxlen = MAX_LEN, truncating = 'post', padding = 'post')
-word_index = tokenizer_obj.word_index
-num_words=len(word_index)+1
-embedding_matrix=np.zeros((num_words,100))
-for word,i in tqdm(word_index.items()):
-    if i > num_words:
-        continue
-    emb_vec=embedding_dict.get(word)
-    if emb_vec is not None:
-        embedding_matrix[i]=emb_vec
-
-# fit model
-model=Sequential()
-embedding=Embedding(num_words,100,embeddings_initializer=Constant(embedding_matrix),input_length=MAX_LEN,trainable=False)
-model.add(embedding)
-model.add(SpatialDropout1D(0.2))
-model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
-optimzer=Adam(learning_rate=1e-5)
-model.compile(loss='binary_crossentropy',optimizer=optimzer,metrics=['accuracy'])
-model.summary()
-train_df=tweet_pad[:train.shape[0]]
-test_df=tweet_pad[train.shape[0]:]
-X_train,X_test,y_train,y_test=train_test_split(train_df,train['target'].values,test_size=0.15)
-history=model.fit(X_train,y_train,batch_size=4,epochs=15,validation_data=(X_test,y_test),verbose=2)
-
-# make test predictions
-y_pre=model.predict(test)
-y_pre=np.round(y_pre).astype(int).reshape(3263)
-sub=pd.DataFrame({'id':sample_submission['id'].values.tolist(),'target':y_pre})
