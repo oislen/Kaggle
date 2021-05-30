5e5344e728069ca1b47cb599d22acbd617d616f6
oislen <oisin.leonard@gmail.com>
2021-05-30 18:45:52 +0100

    updated pip install packages with conda install conda-forge

--- environments/kaggle.bat
+++ environments/kaggle.bat
@@ -4,45 +4,48 @@ call conda env list
 :: create and activate new environment
 call conda env remove --name kaggle
 call conda env list
-::call conda create --name kaggle python=3.6 --yes
-call conda create --name kaggle python --yes
+call conda create --name kaggle python=3.6 --yes
 call conda activate kaggle
 call conda list
-
-:: install all relevant python libraries
-call conda install -c conda-forge numpy --yes
-call conda install -c conda-forge pandas --yes
-call conda install -c conda-forge scipy --yes
-call conda install -c conda-forge statsmodels --yes
-call conda install -c conda-forge scikit-learn --yes
-call conda install -c conda-forge matplotlib --yes
-call conda install -c conda-forge seaborn --yes
-call conda install -c conda-forge keras --yes
-call conda install -c conda-forge spyder --yes
-call conda install -c conda-forge notebook --yes
-call conda install -c conda-forge pyarrow --yes
-call conda install -c conda-forge pygam --yes
-::call conda install -c conda-forge mysqlalchemy --yes
-::call conda install -c conda-forge scrapy --yes
-call conda install -c conda-forge spacy --yes
-call python -m spacy download en_core_web_sm
 
 :: pip install additional libraries
 call pip install --upgrade pip
 call pip install kaggle
-call pip install langdetect
-call pip install pyspellchecker
-call pip install nltk
-call pip install --upgrade gensim
-call pip install sentencepiece
-#call pip install tensorflow
-#call pip install tensorflow_hub
+
+:: install all relevant python libraries
+call conda install -c conda-forge numpy --yes
+call conda install -c conda-forge scipy --yes
+call conda install -c conda-forge pandas --yes
+call conda install -c conda-forge spyder --yes
+call conda install -c conda-forge notebook --yes
+call conda install -c conda-forge pyarrow --yes
+
+:: visualisation libraries
+call conda install -c conda-forge matplotlib --yes
+call conda install -c conda-forge seaborn --yes
+
+:: nlp libraries
+call conda install -c conda-forge langdetect --yes
+call conda install -c conda-forge pyspellchecker --yes
+call conda install -c conda-forge nltk --yes
+call conda install -c conda-forge gensim --yes
+call conda install -c conda-forge sentencepiece --yes
+call conda install -c conda-forge spacy --yes
+call python -m spacy download en_core_web_sm
+
+:: ml libraries
+call conda install -c conda-forge statsmodels --yes
+call conda install -c conda-forge scikit-learn --yes
+call conda install -c conda-forge pygam --yes
+call conda install -c conda-forge keras --yes
+:: call conda install -c conda-forge tensorflow --yes
+:: call conda install -c conda-forge tensorflow-hub --yes
 
 :: list all installed libraries
 call conda list
 
 :: export to yml file
-:: conda env export > aws.yml
-:: conda env create -f aws.yml
+:: conda env export > kaggle.yml
+:: conda env create -f kaggle.yml
 
 ::call conda deactivate
--- environments/kaggle.sh
+++ environments/kaggle.sh
@@ -4,45 +4,48 @@ conda env list
 # create and activate new environment
 conda env remove --name kaggle
 conda env list
-#conda create --name kaggle python=3.6 --yes
-conda create --name kaggle --yes
+conda create --name kaggle python=3.6 --yes
 conda activate kaggle
 conda list
-
-# install all relevant python libraries
-conda install -c conda-forge numpy --yes
-conda install -c conda-forge pandas --yes
-conda install -c conda-forge scipy --yes
-conda install -c conda-forge statsmodels --yes
-conda install -c conda-forge scikit-learn --yes
-conda install -c conda-forge matplotlib --yes
-conda install -c conda-forge seaborn --yes
-conda install -c conda-forge keras --yes
-conda install -c conda-forge spyder --yes
-conda install -c conda-forge notebook --yes
-conda install -c conda-forge pyarrow --yes
-conda install -c conda-forge pygam --yes
-#conda install -c conda-forge mysqlalchemy --yes
-#conda install -c conda-forge scrapy --yes
-conda install -c conda-forge spacy --yes
-python -m spacy download en_core_web_sm
 
 # pip install additional libraries
 pip install --upgrade pip
 pip install kaggle
-pip install langdetect
-pip install pyspellchecker
-pip install nltk
-pip install --upgrade gensim
-pip install sentencepiece
-#pip install tensorflow
-#pip install tensorflow_hub
+
+# install all relevant python libraries
+conda install -c conda-forge numpy --yes
+conda install -c conda-forge scipy --yes
+conda install -c conda-forge pandas --yes
+conda install -c conda-forge spyder --yes
+conda install -c conda-forge notebook --yes
+conda install -c conda-forge pyarrow --yes
+
+# visualisation libraries
+conda install -c conda-forge matplotlib --yes
+conda install -c conda-forge seaborn --yes
+
+# nlp libraries
+conda install -c conda-forge langdetect --yes
+conda install -c conda-forge pyspellchecker --yes
+conda install -c conda-forge nltk --yes
+conda install -c conda-forge gensim --yes
+conda install -c conda-forge sentencepiece --yes
+conda install -c conda-forge spacy --yes
+python -m spacy download en_core_web_sm
+
+# ml libraries
+conda install -c conda-forge statsmodels --yes
+conda install -c conda-forge scikit-learn --yes
+conda install -c conda-forge pygam --yes
+conda install -c conda-forge keras --yes
+# conda install -c conda-forge tensorflow --yes
+# conda install -c conda-forge tensorflow-hub --yes
 
 # list all installed libraries
 conda list
 
 # export to yml file
-# conda env export > aws.yml
-# conda env create -f aws.yml
+# conda env export > kaggle.yml
+# conda env create -f kaggle.yml
 
 #conda deactivate
