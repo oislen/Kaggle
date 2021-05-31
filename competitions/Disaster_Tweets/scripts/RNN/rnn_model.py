6e224e572276085078c0bbf5e6c023dfbbb674bd
oislen <oisin.leonard@gmail.com>
2021-05-31 15:25:58 +0100

    extracted out the rnn archecture from keras_mess.py eda script

---
+++ competitions/Disaster_Tweets/scripts/RNN/rnn_model.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 12:54:32 2021
+
+@author: oislen
+"""
+
+# load in relevant libraries
+from keras.models import Sequential
+from keras.layers import Embedding, Dense, LSTM,SpatialDropout1D
+from keras.initializers import Constant
+
+def rnn_model(embedding_matrix,
+              input_length):
+    
+    """
+        
+    """
+    
+    # set the dropout rate
+    dropout_rate = 0.2
+    
+    # initiate the sequence model
+    model = Sequential()
+    
+    # create an embeddings layer
+    embeddings = Embedding(input_dim = embedding_matrix.shape[0],
+                           output_dim = embedding_matrix.shape[1],
+                           embeddings_initializer = Constant(embedding_matrix),
+                           input_length = input_length,
+                           trainable = False
+                           )
+    # add the embeddings to the sequence model
+    model.add(embeddings)
+    
+    # create a spatial dropout layer
+    spatial_dropout = SpatialDropout1D(dropout_rate)
+    # add the spatial dropout to the sequence model
+    model.add(spatial_dropout)
+    
+    # create an LSTM stage
+    lstm = LSTM(units = 64, 
+                dropout = dropout_rate, 
+                recurrent_dropout = 
+                dropout_rate
+                )
+    # add lstm to sequence model
+    model.add(lstm)
+    
+    # create a dense layer
+    dense = Dense(units = 1, 
+                  activation = 'sigmoid'
+                  )
+    # add dense layer to squence model
+    model.add(dense)
+    
+    return model
