c27ec258c4ccdb890ed1b166af19641036a753ec
oislen <oisin.leonard@gmail.com>
2021-06-02 07:35:38 +0100

    extracted out the initial data processing steps including shahules ideas into seperate function

--- competitions/Disaster_Tweets/scripts/edas/keras_mess.py
+++ competitions/Disaster_Tweets/scripts/edas/keras_mess.py
@@ -12,73 +12,26 @@ sys.path.append(os.path.dirname(os.getcwd()))
 import cons
 import pandas as pd
 import numpy as np
-import spacy
-# custom functions
-import shahules_utils as shutils
-from load_glove_word_vecs import load_glove_word_vecs
-from normalise_tweet import normalise_tweet
-from prep_model_corpus import prep_model_corpus
-
-
 from sklearn.model_selection import train_test_split
 from keras.optimizers import Adam
-#from nltk.tokenize import spacy
 
-
+# custom functions
+from load_glove_word_vecs import load_glove_word_vecs
+from prep_model_corpus import prep_model_corpus
+from process_data import process_data
 from rnn_model import rnn_model
 from fit_model import fit_model
-from keras.callbacks import ReduceLROnPlateau
 
-#################
-#-- Data Prep --#
-#################
-
-# load in the raw data files
-train = pd.read_csv(cons.raw_train_fpath)
-test = pd.read_csv(cons.raw_test_fpath)
-sample_submission = pd.read_csv(cons.raw_sample_submission_fpath)
-
-# combine train and test
-train['dataset'] = 'train'
-test['target'] = np.nan
-test['dataset'] = 'test'
-data = pd.concat(objs = [train, test], ignore_index = True)
-
-# if running shahules data prep steps
-if True:
-    
-    # apply data cleaning
-    data['text'] = data['text'].apply(lambda x : shutils.remove_URL(x))
-    data['text'] = data['text'].apply(lambda x : shutils.remove_html(x))
-    data['text'] = data['text'].apply(lambda x: shutils.remove_emoji(x))
-    data['text'] = data['text'].apply(lambda x : shutils.remove_punct(x))
-    
-    # create corpus object given available word vectors
-    corpus = shutils.create_corpus(df = data, text_col = 'text')
-
-# using custom data prep steps
-else:
-    
-    # create spacy instance 
-    nlp = spacy.load('en_core_web_sm')
-    
-    # run text normalisation function
-    data['text_norm'] = data['text'].apply(lambda x: normalise_tweet(tweet = x, nlp = nlp, norm_configs = cons.norm_configs))
-    
-    # remove special @ characters
-    # could replace cases with @user?
-    #data['text_norm_clean'] = data['text_norm'].apply(lambda x: re.sub('@', '', x))
-
-    # create a corpus to feed into the model
-    corpus = [sent.split() for sent in data['text_norm'].to_list()]
+# run data procressing
+corpus, data = process_data(shahules = True)
 
 # load in glove word vectors
 # create embedding dictionary
-embedding_dict = load_glove_word_vecs(cons.glove_100d_fpath)
+embedding_dict, data = load_glove_word_vecs(cons.glove_100d_fpath)
 
-###################
-#-- Keras Model --#
-###################
+# split data into train and test sets
+train_filter = data['dataset'] == 'train'
+train = data.loc[train_filter, :]
 
 # run prep model corpus
 tweet_pad, embedding_matrix, max_len = prep_model_corpus(corpus = corpus, 
@@ -105,14 +58,6 @@ model = rnn_model(embedding_matrix = embedding_matrix,
 # define optimiser and compile
 optimizer = Adam(learning_rate = 1e-5)
 
-# set a learning rate annealer
-learning_rate_reduction = ReduceLROnPlateau(monitor = 'val_accuracy', 
-                                            patience = 3, 
-                                            verbose = 1, 
-                                            factor = 0.5, 
-                                            min_lr = 0.00001
-                                            )
-
 # Attention: Windows implementation may cause an error here. In that case use model_name=None.
 fit_model(model = model, 
           epochs = 15,
@@ -123,7 +68,6 @@ fit_model(model = model,
           optimizer = optimizer,
           metric = 'accuracy',
           loss = 'binary_crossentropy',
-          #lrate_red = learning_rate_reduction,
           X_train = X_train,
           X_val = X_valid, 
           Y_train = y_train, 
@@ -131,6 +75,7 @@ fit_model(model = model,
           )
 
 # make test predictions
+sample_submission = pd.read_csv(cons.raw_sample_submission_fpath)
 test_df=tweet_pad[train.shape[0]:]
 y_pre=model.predict(test_df)
 y_pre = np.round(y_pre).astype(int).reshape(3263)
